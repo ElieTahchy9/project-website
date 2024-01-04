7622d6eef17ac8c366f9894483e47553c775b6fc
ElieTahchy9 <148841562+ElieTahchy9@users.noreply.github.com>
2024-01-04 12:42:27 +0200

    Update App.js

--- Journey_leb/src/App.js
+++ Journey_leb/src/App.js
@@ -14,6 +14,24 @@ import About from './componnents/About/About'
 // import Footer from './componnents/Footer/Footer';
 import Contact from './componnents/Contact/Contact';
 import CityDetails from './componnents/CityDetails';
+import Saida from './componnents/CityDetails/Saida';
+import Jbeil from './componnents/CityDetails/Jbeil';
+import Zahle from './componnents/CityDetails/Zahle';
+import Baalbak from './componnents/CityDetails/Baalbak';
+import Bcharre from './componnents/CityDetails/Bcharre';
+import Faraya from './componnents/CityDetails/Faraya';
+import Laqlouq from './componnents/CityDetails/Laqlouq';
+import Harissa from './componnents/CityDetails/Harissa';
+import Batroun from './componnents/CityDetails/Batroun';
+import Qoubayat from './componnents/CityDetails/Qoubayat';
+import Beirut from './componnents/CityDetails/Beirut';
+import ActivitySaida from './componnents/ActivityDetails/Saida/ActivitySaida';
+import SecondActivitySaida from './componnents/ActivityDetails/Saida/SecondActivitySaida';
+import ActivityJbeil from './componnents/ActivityDetails/Jbeil/ActivityJbeil';
+import SecondActivityJbeil from './componnents/ActivityDetails/Jbeil/SecondActivityJbeil';
+import ActivityQoubayat from './componnents/ActivityDetails/Qoubayat/ActivityQoubayat';
+import SecondActivityQoubayat from './componnents/ActivityDetails/Qoubayat/SecondActivityQoubayat';
+import ThirdActivityQoubayat from './componnents/ActivityDetails/Qoubayat/ThirdActivityQoubayat';
 
 function App() {
   return (
@@ -32,6 +50,25 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path='/PaymentDetails' element={<PaymentDetails />} />
+        <Route path="/cityDetails/Saida" element={<Saida />} />
+        <Route path="/cityDetails/Jbeil" element={<Jbeil />} />
+        <Route path="/cityDetails/Zahle" element={<Zahle />} />
+        <Route path="/cityDetails/Baalbak" element={<Baalbak />} />
+        <Route path="/cityDetails/Bcharre" element={<Bcharre />} />
+        <Route path="/cityDetails/Faraya" element={<Faraya />} />
+        <Route path="/cityDetails/Laqlouq" element={<Laqlouq/>} />
+        <Route path="/cityDetails/Harissa" element={<Harissa/>} />
+        <Route path="/cityDetails/Batroun" element={<Batroun/>} /> 
+        <Route path="/cityDetails/Qoubayat" element={<Qoubayat/>} /> 
+        <Route path="/cityDetails/Beirut" element={<Beirut/>} />
+        <Route path="/activity/Saida/1" element={<ActivitySaida />} />
+        <Route path="/activity/Saida/2" element={<SecondActivitySaida />} />
+        <Route path="/activity/Jbeil/1" element={<ActivityJbeil />} />
+        <Route path="/activity/Jbeil/2" element={<SecondActivityJbeil />} /> 
+        <Route path="/activity/Qoubayat/1" element={<ActivityQoubayat />} /> 
+        <Route path="/activity/Qoubayat/2" element={<SecondActivityQoubayat />} /> 
+        <Route path="/activity/Qoubayat/3" element={<ThirdActivityQoubayat />} /> 
+
       </Routes>
   
       
